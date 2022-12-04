86457ac2bcdad940cfacc763c8d5f8dc20c8bad7
wmgaertner <gaertner_william@outlook.com>
2022-12-04 00:43:08 -0500

    submit day 4 solutions

---
+++ day4/day4p2.py
@@ -0,0 +1,18 @@
+import sys
+file = open(sys.argv[1])
+
+count = 0
+for line in file:
+    sections = line.strip().split(',')
+    section1 = list(map(int,sections[0].split('-')))
+    section2 = list(map(int, sections[1].split('-')))
+    if section1[0] <= section2[0] and section1[1] >= section2[1]:
+        print(section1, ' ', section2)
+        count+=1
+    elif section2[0] <= section1[0] and section2[1] >= section1[1]:
+        print(section1, ' ', section2)
+        count+=1
+    
+
+print(count)
+    
