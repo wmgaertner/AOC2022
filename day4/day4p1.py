5a53e7fee603bb728ed2ed6c062fee8a9e5d2417
wmgaertner <gaertner_william@outlook.com>
2022-12-04 00:43:53 -0500

    forgot to remove print statements

--- day4/day4p1.py
+++ day4/day4p1.py
@@ -7,10 +7,10 @@ for line in file:
     section1 = list(map(int,sections[0].split('-')))
     section2 = list(map(int, sections[1].split('-')))
     if section1[0] <= section2[0] and section1[1] >= section2[0]:
-        print(section1, ' ', section2)
+        
         count+=1
     elif section2[0] <= section1[0] and section2[1] >= section1[0]:
-        print(section1, ' ', section2)
+        
         count+=1
     
 
--- day4/day4p2.py
+++ day4/day4p2.py
@@ -7,10 +7,8 @@ for line in file:
     section1 = list(map(int,sections[0].split('-')))
     section2 = list(map(int, sections[1].split('-')))
     if section1[0] <= section2[0] and section1[1] >= section2[1]:
-        print(section1, ' ', section2)
         count+=1
     elif section2[0] <= section1[0] and section2[1] >= section1[1]:
-        print(section1, ' ', section2)
         count+=1
     
 
